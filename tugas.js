ab438bd504a8418fd67f30de64a7a90444d1c39e
safaimasania <safaimasania@gmail.com>
2025-01-15 11:33:29 +0700

    Add files via upload

---
+++ tugas.js
@@ -0,0 +1,60 @@
+let namaDepan = "Safa";
+let namaTengah = "Ima";
+let namaBelakang = "Sania";
+
+document.write(" " + "nama saya adalah " + namaDepan + " " + namaTengah + " " + namaBelakang);
+
+let alamatDepan ="DsBlerong";
+let alamatTengah ="rt 02/04";
+let alamatBelakang ="GunturDemak";
+
+document.write(" " + "saya tinggal di " + alamatDepan + " " + alamatTengah + " " + alamatBelakang);
+
+let usia ="15";
+
+document.write(" " + "pada tahun ini usia saya adalah " + usia);
+
+let ttlTempat = "demak";
+let ttlTanggal = "07-02-2009";
+
+document.write(" " + "saya lahir di " + " " + ttlTempat + " " + ttlTanggal);
+
+let gender = "wanita";
+
+document.write(" " + "jenis kelamin saya adalah " + " " + gender);
+
+let agama = "islam";
+
+document.write(" " + "saya beragama " + " " + agama);
+
+let email = "safasania817@gmail.com";
+
+document.write(" " + "Alamat email saya adalah " + " " + email);
+
+let berat = "34";
+
+document.write(" " + "saya memiliki berat badan " + " " + berat);
+
+let tinggi ="150 cm";
+
+document.write(" " + "kg dengan tinggi" + " " + tinggi);
+
+let hobi ="bermain";
+
+document.write( " " + "saya sangat senang " + hobi);
+
+let cita ="dokter";
+
+document.write(" " + "karena saya memiliki cita-cita " + " " + cita);
+
+let pekerjaan ="pelajar";
+
+document.write(" " + "saat ini saya adalah seorang " + " " + pekerjaan);
+
+let sekolah ="Smkn1Sayung";
+
+document.write(" " + "yang bersekolah di " + " " + sekolah);
+
+let jurusan ="PPLG";
+
+document.write(" " + "pada jurusan " + " " + jurusan);
